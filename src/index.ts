4b226fd0b35a19b9aabe9a0226d0259b40014b63
Tony Hirst <tony.hirst@gmail.com>
2022-04-27 15:52:22 +0100

    First attempt

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,77 @@
+
+   
 import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+//https://jupyterlab.readthedocs.io/en/3.1.x/api/classes/notebook.notebookactions-1.html
+import { NotebookActions } from '@jupyterlab/notebook';
+//import { IKernelConnection } from '@jupyterlab/services/Kernel IKernelConnection';
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
 /**
- * Initialization data for the jupyterlab_cell_status_extension extension.
+ * Initialization data for the jupyterlab-cell-flash extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab_cell_status_extension:plugin',
+  activate,
+  id: 'jupyterlab_cell_status:plugin',
   autoStart: true,
-  optional: [ISettingRegistry],
-  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
-    console.log('JupyterLab extension jupyterlab_cell_status_extension is activated!');
+  optional: [ISettingRegistry],//, IKernelConnection]
+};
 
+function activate (
+  app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null): void {
+    console.log("jupyterlab_cell_status:plugin activating...");
     if (settingRegistry) {
       settingRegistry
         .load(plugin.id)
         .then(settings => {
-          console.log('jupyterlab_cell_status_extension settings loaded:', settings.composite);
+          const root = document.documentElement;
+          const updateSettings = (): void => {
+            const queue_color = settings.get('status_queue').composite as string;
+            const success_color = settings.get('status_success').composite as string;
+            const error_color = settings.get('status_error').composite as string;
+            root.style.setProperty('--jp-cell-status-queue', queue_color);
+            root.style.setProperty('--jp-cell-status-success', success_color);
+            root.style.setProperty('--jp-cell-status-error', error_color);
+          };
+          updateSettings();
+          // We can auto update the color
+          settings.changed.connect(updateSettings);
         })
         .catch(reason => {
           console.error('Failed to load settings for jupyterlab_cell_status_extension.', reason);
-        });
+      });
     }
+
+    /*
+    IKernelConnection.connectionStatusChanged.connect((kernel, conn_stat) => {
+
+    console.log("KERNEL ****"+conn_stat)
+    });
+    */
+    NotebookActions.executed.connect((_, args) => {
+      const { cell } = args;
+      const { success } = args;
+      if (cell.model.type == 'code') {
+        if (success)
+          cell.inputArea.promptNode.classList.add("executed-success");
+        else
+          cell.inputArea.promptNode.classList.add("executed-error");
+          cell.inputArea.promptNode.classList.remove("scheduled");
+      }
+    });
+
+    NotebookActions.executionScheduled.connect((_, args) => {
+      const { cell } = args;
+      if (cell.model.type == 'code') {
+        cell.inputArea.promptNode.classList.remove("executed");
+        cell.inputArea.promptNode.classList.add("scheduled");
+      }
+    });  
+
+    console.log("jupyterlab_cell_status:plugin activated...");
   }
-};
 
 export default plugin;
